d7d9d37f86d4350820f70717a5f9f40cb9e39613
Dennis Kalt <denniskalt@hotmail.com>
2017-04-18 11:18:21 +0200

    Aktuelle Datenbank

--- sql/php-praktikum.sql
+++ sql/php-praktikum.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 11. Apr 2017 um 19:03
+-- Erstellungszeit: 18. Apr 2017 um 11:17
 -- Server-Version: 10.1.19-MariaDB
 -- PHP-Version: 5.5.38
 
@@ -27,8 +27,6 @@ USE `php-praktikum`;
 --
 -- Tabellenstruktur für Tabelle `cities`
 --
--- Erstellt am: 11. Apr 2017 um 16:56
---
 
 CREATE TABLE `cities` (
   `idCities` char(5) NOT NULL,
@@ -39,8 +37,6 @@ CREATE TABLE `cities` (
 
 --
 -- Tabellenstruktur für Tabelle `comments`
---
--- Erstellt am: 11. Apr 2017 um 16:52
 --
 
 CREATE TABLE `comments` (
@@ -55,21 +51,25 @@ CREATE TABLE `comments` (
 --
 -- Tabellenstruktur für Tabelle `confirmcodes`
 --
--- Erstellt am: 11. Apr 2017 um 16:47
---
 
 CREATE TABLE `confirmcodes` (
-  `idConfirmcodes` int(11) NOT NULL,
+  `idConfirmcodes` char(12) NOT NULL,
   `usersId` int(11) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `pwreset` char(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `confirmcodes`
+--
+
+INSERT INTO `confirmcodes` (`idConfirmcodes`, `usersId`, `created`, `pwreset`) VALUES
+('cYxbp5sS79gx', 4, '2017-04-18 09:09:24', '5xwgt89E3eAQ');
 
 -- --------------------------------------------------------
 
 --
 -- Tabellenstruktur für Tabelle `hashtags`
---
--- Erstellt am: 11. Apr 2017 um 16:49
 --
 
 CREATE TABLE `hashtags` (
@@ -82,8 +82,6 @@ CREATE TABLE `hashtags` (
 --
 -- Tabellenstruktur für Tabelle `hashtagsposts`
 --
--- Erstellt am: 11. Apr 2017 um 16:50
---
 
 CREATE TABLE `hashtagsposts` (
   `hashtagsId` int(11) NOT NULL,
@@ -95,8 +93,6 @@ CREATE TABLE `hashtagsposts` (
 --
 -- Tabellenstruktur für Tabelle `login_attempts`
 --
--- Erstellt am: 11. Apr 2017 um 16:47
---
 
 CREATE TABLE `login_attempts` (
   `usersId` int(11) NOT NULL,
@@ -106,9 +102,27 @@ CREATE TABLE `login_attempts` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `posts`
+-- Tabellenstruktur für Tabelle `options`
 --
--- Erstellt am: 11. Apr 2017 um 16:53
+
+CREATE TABLE `options` (
+  `idOptions` int(11) NOT NULL,
+  `usersId` int(11) NOT NULL,
+  `bg` varchar(60) NOT NULL,
+  `design` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `options`
+--
+
+INSERT INTO `options` (`idOptions`, `usersId`, `bg`, `design`) VALUES
+(1, 4, 'way.jpg', 'default');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `posts`
 --
 
 CREATE TABLE `posts` (
@@ -123,20 +137,24 @@ CREATE TABLE `posts` (
 --
 -- Tabellenstruktur für Tabelle `status`
 --
--- Erstellt am: 11. Apr 2017 um 16:58
---
 
 CREATE TABLE `status` (
   `idStatus` int(11) NOT NULL,
   `beschreibung` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Daten für Tabelle `status`
+--
+
+INSERT INTO `status` (`idStatus`, `beschreibung`) VALUES
+(1, 'inaktiv'),
+(2, 'aktiv');
+
 -- --------------------------------------------------------
 
 --
 -- Tabellenstruktur für Tabelle `users`
---
--- Erstellt am: 11. Apr 2017 um 16:46
 --
 
 CREATE TABLE `users` (
@@ -153,15 +171,24 @@ CREATE TABLE `users` (
   `birthDate` date NOT NULL,
   `postcode` char(5) NOT NULL,
   `usersTypesId` int(11) NOT NULL,
-  `url` varchar(40) NOT NULL
+  `url` varchar(40) NOT NULL,
+  `lastLogin` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `users`
+--
+
+INSERT INTO `users` (`idUsers`, `username`, `email`, `password`, `salt`, `regDate`, `status`, `profilepic`, `forename`, `surname`, `birthDate`, `postcode`, `usersTypesId`, `url`, `lastLogin`) VALUES
+(4, 'tester', 'test@test.de', '$2y$07$JDJ5JDA3dGVzakpCUE5vYuTTpGaWR1/ztmBPkCCqPuplsgwULKRuq', '$2y$07tesjJBPNocKGKz-d', '2017-04-11 20:40:54', 1, 'bg/way.jpg', 'Max', 'Mustermann', '1995-07-10', '31785', 0, 'tester', '0000-00-00'),
+(5, 'dennis', 'dennis@test.de', '$2y$07$JDJ5JDA3ZGVuKcJmdVpuN.19XB/ukF0zzKtxhU0nqC006H1JqoMIu', '$2y$07den)ÂfuZn4AOz@n6', '2017-04-15 11:51:16', 2, '', '', '', '0000-00-00', '', 4, '', '0000-00-00'),
+(6, 'Cat', 'd@t.de', '$2y$07$JDJ5JDA3ZEB0UkFZRDZAQOeThTot/kvGFdXelMKGh9J9WLaxXJUlW', '$2y$07d@tRAYD6@AF1-nQC', '2017-04-16 13:33:30', 1, '', '', '', '0000-00-00', '', 0, '', '0000-00-00'),
+(7, 'test', 't@t.de', '$2y$07$JDJ5JDA3dEB0a0hPZlBIRu5pXP8odJos4tzmkYhbWyAAyPFhjdEBu', '$2y$07t@tkHOfPHGjXIYFW', '2017-04-18 08:10:08', 2, '', '', '', '0000-00-00', '', 0, '', '0000-00-00');
 
 -- --------------------------------------------------------
 
 --
 -- Tabellenstruktur für Tabelle `userstypes`
---
--- Erstellt am: 11. Apr 2017 um 16:49
 --
 
 CREATE TABLE `userstypes` (
@@ -169,12 +196,18 @@ CREATE TABLE `userstypes` (
   `type` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Daten für Tabelle `userstypes`
+--
+
+INSERT INTO `userstypes` (`idUsersTypes`, `type`) VALUES
+(1, 'Administrator'),
+(4, 'Nutzer');
+
 -- --------------------------------------------------------
 
 --
 -- Tabellenstruktur für Tabelle `votes`
---
--- Erstellt am: 11. Apr 2017 um 16:55
 --
 
 CREATE TABLE `votes` (
@@ -219,6 +252,12 @@ ALTER TABLE `login_attempts`
   ADD PRIMARY KEY (`usersId`);
 
 --
+-- Indizes für die Tabelle `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`idOptions`);
+
+--
 -- Indizes für die Tabelle `posts`
 --
 ALTER TABLE `posts`
@@ -258,15 +297,15 @@ ALTER TABLE `votes`
 ALTER TABLE `comments`
   MODIFY `idComments` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT für Tabelle `confirmcodes`
---
-ALTER TABLE `confirmcodes`
-  MODIFY `idConfirmcodes` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT für Tabelle `hashtags`
 --
 ALTER TABLE `hashtags`
   MODIFY `idHashtags` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `options`
+--
+ALTER TABLE `options`
+  MODIFY `idOptions` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT für Tabelle `posts`
 --
@@ -276,17 +315,17 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT für Tabelle `status`
 --
 ALTER TABLE `status`
-  MODIFY `idStatus` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idStatus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `idUsers` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idUsers` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT für Tabelle `userstypes`
 --
 ALTER TABLE `userstypes`
-  MODIFY `idUsersTypes` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idUsersTypes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT für Tabelle `votes`
 --
